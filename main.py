03ea4312d8d995a00c65da1939a33edda5e18f86
MiekevdMeiden <50616839+MiekevdMeiden@users.noreply.github.com>
2021-10-19 15:24:58 +0200

    basic framework

--- main.py
+++ main.py
@@ -6,3 +6,20 @@
 
 # The rest of the code
 
+class Agent:
+
+    # constructor
+    def __init__(self, i,  I, O, S_prime):
+        self.id = i
+
+
+class Environment:
+    # constructor
+    def __init__(self):
+        # some FSM like structure
+
+
+
+
+
+## all actions go down here
